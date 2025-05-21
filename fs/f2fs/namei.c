8717f663b1afa27c87d8a3d415e1e437e6938f78
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-05-21 18:19:33 +0545

    patch-4.19.154-155

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -482,6 +482,7 @@ static struct dentry *f2fs_lookup(struct inode *dir, struct dentry *dentry,
 	nid_t ino = -1;
 	int err = 0;
 	unsigned int root_ino = F2FS_ROOT_INO(F2FS_I_SB(dir));
+	struct fscrypt_name fname;
 	struct f2fs_filename fname;
 
 	trace_f2fs_lookup_start(dir, dentry, flags);
@@ -491,14 +492,13 @@ static struct dentry *f2fs_lookup(struct inode *dir, struct dentry *dentry,
 		goto out;
 	}
 
-	err = f2fs_prepare_lookup(dir, dentry, &fname);
-	generic_set_encrypted_ci_d_ops(dir, dentry);
+	err = fscrypt_prepare_lookup(dir, dentry, &fname);
 	if (err == -ENOENT)
 		goto out_splice;
 	if (err)
 		goto out;
 	de = __f2fs_find_entry(dir, &fname, &page);
-	f2fs_free_filename(&fname);
+	fscrypt_free_filename(&fname);
 
 	if (!de) {
 		if (IS_ERR(page)) {
