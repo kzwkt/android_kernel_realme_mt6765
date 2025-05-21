1d777def57482f80d9ee4a0939a73b142e5df340
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-05-21 16:09:47 +0545

    patch-4.19.139-140

--- arch/arm/kernel/stacktrace.c
+++ arch/arm/kernel/stacktrace.c
@@ -34,6 +34,19 @@
  *	ldm	{..., fp, pc}
  *
  *
+ * When compiled with clang, pc and sp are not pushed. A simple function
+ * prologue looks like this when built with clang:
+ *
+ *	stmdb	{..., fp, lr}
+ *	add	fp, sp, #x
+ *	sub	sp, sp, #y
+ *
+ * A simple function epilogue looks like this when built with clang:
+ *
+ *	sub	sp, fp, #x
+ *	ldm	{..., fp, pc}
+ *
+ *
  * Note that with framepointer enabled, even the leaf functions have the same
  * prologue and epilogue, therefore we can ignore the LR value in this case.
  */
